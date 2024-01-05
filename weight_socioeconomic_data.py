3b8c8a753f2860bcc11a975ad6ba3c1f73048456
maxxxbb <82452765+maxxxbb@users.noreply.github.com>
2024-01-05 14:49:40 +0100

    Added Park Coverage to socioeconomic covariates

--- weight_socioeconomic_data.py
+++ weight_socioeconomic_data.py
@@ -1,5 +1,9 @@
 import pandas as pd
 import geopandas as gpd
+import numpy as np
+from shapely.wkt import loads
+
+#### FUNCTIONS
 
 def calculate_weighted_socioeconomic_data(demographics_csv, taxi_zones_csv):
     """
@@ -62,11 +66,63 @@ def calculate_weighted_socioeconomic_data(demographics_csv, taxi_zones_csv):
         socioeconomic_variables
     )
 
-    final_df = pd.DataFrame(taxi_zone_data)
+    taxi_zones_socioeconomics = pd.DataFrame(taxi_zone_data)
+    
+    # set covariates to N.A.N if the sum of intersection shares is less than 0.2
+    
+    taxi_zones_socioeconomics['sum_of_shares'] = taxi_zones_socioeconomics['intersection_shares'].apply(convert_to_floats_and_sum)
 
-    output_path = "taxi_zones_with_socioeconomics_popweighted.csv"
+
+    exclude_columns = ['LocationID', 'Zone', 'ZCTA_IDS' , 'sum_of_shares']
+
+
+    columns_to_nan = taxi_zones_socioeconomics.columns.difference(exclude_columns)
+
+    taxi_zones_socioeconomics.loc[taxi_zones_socioeconomics['sum_of_shares'] < 0.2, columns_to_nan] = np.nan
+    
+    final_df = taxi_zones_socioeconomics
+    
+
+    output_path = "ACS_data/taxi_zones_ACS.csv"
     final_df.to_csv(output_path, index=False)
 
+
+
+def convert_to_floats_and_sum(string):
+    """
+    Helper function to detect taxi zones which do not correspond to a single Zip Code
+    Area from the ACS data. The covariates of taxi zones which intersect with the zones 
+    less than 20 percent are set then later set to N.A.N
+    """
+    if pd.isna(string) or string in ['NaN', 'nan', '']:
+        return 0
+    try:
+        # Splitting the string (containing intersection shares of zctas corresponding to one taxi zones.) by comma and converting each part to a float
+        numbers = [float(num) for num in string.split(',')]
+        
+        return sum(numbers)
+    except ValueError:
+        return 0
+
+
+def calculate_park_area_in_taxizone(taxizone, parks):
+    """
+    Helper Function which returns the sum of park areas in a given taxizone,
+    by calculating the intersection of each park geometry with the given taxizone geometry.
+
+    Function will be applied to each row of the taxizone GeoDataFrame
+
+    Parameters:
+    taxizone (GeoDataFrame): a single row of the taxizone GeoDataFrame
+    parks (GeoDataFrame): the parks GeoDataFrame
+
+    Returns:
+    float: the sum of park areas in the taxizone
+
+    """
+    
+    intersections = parks['multipolygon'].intersection(taxizone['geometry'])
+    return intersections.area.sum()
 
 
 
@@ -105,8 +161,8 @@ def _compute_weighted_averages(taxi_zones_gdf, demographics_gdf, socioeconomic_v
                 weighted_proportion = area_proportion * population_weight
                 intersecting_zctas.append((zcta, weighted_proportion))
                 zcta_ids.append(zcta.zcta)
-                intersection_shares.append((zcta.zcta, area_proportion)) 
-
+                # intersection_shares.append((zcta.zcta, area_proportion)) 
+                intersection_shares.append((area_proportion)) 
         # Calculate real weighted averages
         for var in socioeconomic_variables:
             
@@ -117,12 +173,67 @@ def _compute_weighted_averages(taxi_zones_gdf, demographics_gdf, socioeconomic_v
 
         taxi_zone_socioeconomic_data['ZCTA_IDs'] = ', '.join(map(str, zcta_ids))
         taxi_zone_socioeconomic_data['intersection_shares'] = ', '.join(map(str, intersection_shares))
+        
         taxi_zone_data.append(taxi_zone_socioeconomic_data)
 
     return taxi_zone_data
 
 
+
+def add_parks(parks_csv, taxi_zones_geometry , taxi_zones_ACS):
+    """
+    Add parks data to the taxi zones dataset.
+
+    This function reads parks data and taxi zone geometries from CSV files, 
+    converts them into GeoDataFrames, and adds the park coverage to ACS covariates.
+
+    Parameters:
+    parks_csv (str): File path to the CSV containing parks data.
+    taxi_zones_csv (str): File path to the CSV containing taxi zone geometries.
+
+    Returns:
+    pandas.DataFrame: A DataFrame containing the weighted socioeconomic data by taxi zone.
+    """
+    taxi_zones_ACS = pd.read_csv(taxi_zones_ACS)
+    df_taxizones = pd.read_csv(taxi_zones_geometry)
+    df_parks = pd.read_csv(parks_csv)
+
+    # convert string columns to vector geometries
+    df_parks['multipolygon'] = df_parks['multipolygon'].apply(loads)
+    df_taxizones['geometry'] = df_taxizones['geometry'].apply(loads)
+
+    gdf_parks = gpd.GeoDataFrame(df_parks, geometry='multipolygon')
+    gdf_taxizones = gpd.GeoDataFrame(df_taxizones, geometry='geometry')
+
+    # set coordinate reference system for both such that they coincide
+    gdf_parks.set_crs(epsg=4326, inplace=True)
+    gdf_taxizones.set_crs(epsg=4326, inplace=True)
+    
+    # Apply helper function to each row in taxi zones df
+    gdf_taxizones['park_area'] = gdf_taxizones.apply(lambda x: calculate_park_area_in_taxizone(x, gdf_parks), axis=1)
+    gdf_taxizones['park_coverage'] = gdf_taxizones['park_area'] / gdf_taxizones['shape_area'] * 100
+
+    park_coverage = pd.DataFrame(gdf_taxizones[['park_coverage', 'location_i']])
+
+    taxizone_ACS_parks = pd.merge(taxi_zones_ACS, park_coverage, left_on = 'LocationID',right_on='location_i', how='left').drop(columns=['location_i'])
+
+
+    # Save the dataset
+    output_path = "ACS_data/taxi_zones_ACS_parks.csv"
+    taxizone_ACS_parks.to_csv(output_path, index=False)
+
+
+#### PATHS
+
 demographics_csv = "ACS_data/census_data_zcta.csv"
 taxi_zones_csv = "Shapefiles/taxi_zones_geometry.csv"
+parks_csv = "Heat_Vulnerability/Parks_Properties_20231208.csv"
+taxi_zones_ACS = "ACS_data/taxi_zones_ACS.csv"
+taxi_zones_geometry = "Shapefiles/taxi_zones_geometry.csv"
+parks_csv = "Heat_Vulnerability/Parks_Properties_20231208.csv"
+
+
+#### RUN
 
 calculate_weighted_socioeconomic_data(demographics_csv, taxi_zones_csv)
+add_parks(parks_csv, taxi_zones_geometry , taxi_zones_ACS)
